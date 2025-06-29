db04fd3261327638ad674fbb1667dfa5fd0c8add
BAHMAN <bahmansnights@gmail.com>
2025-06-29 16:33:07 +0430

    init User Entity

---
+++ Mag-Blog.DataLayer/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Mag_Blog.DataLayer.Entities;
+
+public class User
+{
+    public int  Id { get; set; }
+    public string UserName { get; set; }
+    public string FullName { get; set; }
+    public string Password    { get; set; }
+    public UserRole Role { get; set; }
+}
+
+public enum UserRole
+{
+    Admin,
+    User,
+    Author
+}
