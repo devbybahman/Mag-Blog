294a2bf9993ee285e5035e64c21b16d156e603f5
BAHMAN <bahmansnights@gmail.com>
2025-06-29 21:02:25 +0430

    _Layout PartialBandi

---
+++ Mag-Blog.Web/Pages/Shared/_FooterPartial.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mag_Blog.Web.Pages.Shared;
+
+public class _FooterPartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
