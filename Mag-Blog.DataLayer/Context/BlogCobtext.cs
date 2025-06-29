2d238a5b4059ec651290fc97ac6a69dfd5e82cec
BAHMAN <bahmansnights@gmail.com>
2025-06-29 18:01:31 +0430

    Add DbContext

---
+++ Mag-Blog.DataLayer/Context/BlogCobtext.cs
@@ -0,0 +1,17 @@
+﻿using Mag_Blog.DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mag_Blog.DataLayer.Context;
+
+public class BlogCobtext:DbContext
+{
+    public BlogCobtext(DbContextOptions<BlogCobtext> options):base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; }  
+    public DbSet<PostComment> PostComments { get; set; }  
+    public DbSet<Category> Categories { get; set; }  
+    public DbSet<User> Users { get; set; }  
+    
+}
