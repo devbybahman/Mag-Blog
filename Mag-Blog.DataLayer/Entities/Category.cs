288f48267409cead0e3093d8de9ea2d6c4e862d4
BAHMAN <bahmansnights@gmail.com>
2025-06-29 16:30:53 +0430

    init Category Entity

---
+++ Mag-Blog.DataLayer/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Mag_Blog.DataLayer.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public string MetaTag { get; set; }
+    public string MetaDescription { get; set; }
+}
