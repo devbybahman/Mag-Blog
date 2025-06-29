29a4b8366139400d92800777d40d5863ae22032c
BAHMAN <bahmansnights@gmail.com>
2025-06-29 16:35:25 +0430

    init PostComment Entity

---
+++ Mag-Blog.DataLayer/Entities/PostComment.cs
@@ -0,0 +1,10 @@
+﻿namespace Mag_Blog.DataLayer.Entities;
+
+public class PostComment
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public DateTime PublishDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    
+}
