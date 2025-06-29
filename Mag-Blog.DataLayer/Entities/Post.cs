982333f7d0511cc5e0dbfc00e9dbedd1d961ca89
BAHMAN <bahmansnights@gmail.com>
2025-06-29 16:29:08 +0430

    add Post Entity

---
+++ Mag-Blog.DataLayer/Entities/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace Mag_Blog.DataLayer.Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Slug { get; set; }
+    public int Visited { get; set; }
+    
+}
