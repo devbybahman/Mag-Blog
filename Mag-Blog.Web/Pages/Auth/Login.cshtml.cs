6f96dc94dba0e6c55116242ead9892f0103c6d79
BAHMAN <bahmansnights@gmail.com>
2025-06-30 13:37:51 +0430

    Add Login and Register RazorPage

---
+++ Mag-Blog.Web/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mag_Blog.Web.Pages.Auth;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        ViewData["Title"] = "ورود";
+    }
+}
---
+++ Mag-Blog.Web/Pages/Auth/Register.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mag_Blog.Web.Pages.Auth;
+
+public class Register : PageModel
+{
+    public void OnGet()
+    {
+        ViewData["Title"] = "ثبت نام";
+    }
+}
