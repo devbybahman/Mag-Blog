65aa1a480aba790d767fefe3121c29d9b62296ab
BAHMAN <bahmansnights@gmail.com>
2025-06-30 15:25:27 +0430

    add OperationResult Type to Interface + Register in UserService isDone

--- Mag-Blog.CoreLayer/Services/Users/IUserService.cs
+++ Mag-Blog.CoreLayer/Services/Users/IUserService.cs
@@ -1,8 +1,9 @@
-﻿using Mag_Blog.CoreLayer.DTOs.Users;
+﻿using CodeYad_Blog.CoreLayer.Utilities;
+using Mag_Blog.CoreLayer.DTOs.Users;
 
 namespace Mag_Blog.CoreLayer.Services.Users;
 
 public interface IUserService
 {
-    void Register(UserRegisterDTO registerDto);
+    OperationResult Register(UserRegisterDTO registerDto);
 }
--- Mag-Blog.CoreLayer/Services/Users/UserService.cs
+++ Mag-Blog.CoreLayer/Services/Users/UserService.cs
@@ -1,6 +1,38 @@
-﻿namespace Mag_Blog.CoreLayer.Services.Users;
+﻿using CodeYad_Blog.CoreLayer.Utilities;
+using Mag_Blog.CoreLayer.DTOs.Users;
+using Mag_Blog.DataLayer.Context;
+using Mag_Blog.DataLayer.Entities;
+
+namespace Mag_Blog.CoreLayer.Services.Users;
 
 public class UserService:IUserService
 {
-    
+    private readonly BlogCobtext _context;
+
+    public UserService(BlogCobtext context)
+    {
+        _context = context;
+    }
+
+    public OperationResult Register(UserRegisterDTO registerDto)
+    {
+        var SameUser = _context.Users.Any(p => p.UserName == registerDto.UserName);
+        if (SameUser)
+        {
+            return OperationResult.Error("این نام کاربری مجاز نیست");
+        }
+
+        var HashPass = Encoder.EncodeToMd5(registerDto.Password);
+        _context.Users.Add(new User(
+        {
+            UserName = registerDto.UserName,
+            FullName = registerDto.FullName,
+            Password = HashPass,
+            IsDeleted = false,
+            CreationDate = DateTime.Now,
+            Role = UserRole.User
+        });
+        _context.SaveChanges();
+        return OperationResult.Success();
+    }
 }
