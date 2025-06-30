5566743df144dd06682cdfad64846e6e06d80dd5
BAHMAN <bahmansnights@gmail.com>
2025-06-30 14:18:56 +0430

    add UserRegisterDTO

---
+++ Mag-Blog.CoreLayer/DTOs/Users/UserRegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Mag_Blog.CoreLayer.DTOs.Users;
+
+public class UserRegisterDTO
+{
+    public string UserName { get; set; }
+   
+    public string FullName { get; set; }
+
+    public string Password    { get; set; }
+}
