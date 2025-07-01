a5178e7a6dc02c724cecdf4e8c6249aa6280ee63
BAHMAN <bahmansnights@gmail.com>
2025-07-01 11:29:03 +0430

    add ValidateAntiForgeryToken to LoginForm

--- Mag-Blog.Web/Pages/Auth/Login.cshtml.cs
+++ Mag-Blog.Web/Pages/Auth/Login.cshtml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Mag_Blog.Web.Pages.Auth;
 
+[ValidateAntiForgeryToken]
 public class Login : PageModel
 {
     public void OnGet()
