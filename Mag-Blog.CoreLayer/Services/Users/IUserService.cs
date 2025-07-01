a47c476c7476c15794b1d5426fb95969b1c142b2
BAHMAN <bahmansnights@gmail.com>
2025-07-01 11:22:31 +0430

    add UserLoginDTO init to Interface

--- Mag-Blog.CoreLayer/Services/Users/IUserService.cs
+++ Mag-Blog.CoreLayer/Services/Users/IUserService.cs
@@ -6,4 +6,5 @@ namespace Mag_Blog.CoreLayer.Services.Users;
 public interface IUserService
 {
     OperationResult Register(UserRegisterDTO registerDto);
+    OperationResult Login(UserLoginDTO LoginDto);
 }
