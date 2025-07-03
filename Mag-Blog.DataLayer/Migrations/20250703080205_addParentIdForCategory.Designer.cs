ec66f57345fd561f7e3c447e83d7b06decfcb40a
BAHMAN <bahmansnights@gmail.com>
2025-07-03 12:34:24 +0430

    Add-Migration addParentIdForCategory

--- Mag-Blog.DataLayer/Migrations/20250703080205_addParentIdForCategory.Designer.cs
+++ Mag-Blog.DataLayer/Migrations/20250703080205_addParentIdForCategory.Designer.cs
@@ -4,6 +4,7 @@ using Mag_Blog.DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mag_Blog.DataLayer.Migrations
 {
     [DbContext(typeof(BlogCobtext))]
-    partial class BlogCobtextModelSnapshot : ModelSnapshot
+    [Migration("20250703080205_addParentIdForCategory")]
+    partial class addParentIdForCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Mag-Blog.DataLayer/Migrations/20250703080205_addParentIdForCategory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mag_Blog.DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addParentIdForCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Categories");
+        }
+    }
+}
