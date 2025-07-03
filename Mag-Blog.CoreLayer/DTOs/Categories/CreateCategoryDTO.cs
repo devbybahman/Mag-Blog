bd5aaab3af72a74b959332994c79e282d71d7f84
BAHMAN <bahmansnights@gmail.com>
2025-07-03 12:42:49 +0430

    CreateCategoryDTO done

--- Mag-Blog.CoreLayer/DTOs/Categories/CreateCategoryDTO.cs
+++ Mag-Blog.CoreLayer/DTOs/Categories/CreateCategoryDTO.cs
@@ -2,5 +2,13 @@
 
 public class CreateCategoryDTO
 {
+    public string Title { get; set; }
     
+    public string Slug { get; set; }
+    
+    public int? ParentId  { get; set; }
+    
+    public string MetaTag { get; set; }
+    
+    public string MetaDescription { get; set; }
 }
