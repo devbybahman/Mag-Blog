48bf350739c1c5bfe5e8808d4299ec3cc3e21f93
BAHMAN <bahmansnights@gmail.com>
2025-07-03 16:03:08 +0430

    CreateCategory Done

--- Mag-Blog.CoreLayer/Services/Categories/CategoryService.cs
+++ Mag-Blog.CoreLayer/Services/Categories/CategoryService.cs
@@ -13,28 +13,24 @@ public class CategoryService:ICategoryService
     {
         _Context = context;
     }
+
     public OperationResult CreateCategory(CreateCategoryDTO createCategoryDto)
     {
-        var r = _Context.Categories.Any(p => p.Title == createCategoryDto.Title || p.Slug == createCategoryDto.Slug);
-        if (r)
-        {
-            return OperationResult.Error("این دسته بندی از قبل وجود دارد");
-        }
-        
-        
         _Context.Categories.Add(new Category()
-        
+
         {
-        Title = createCategoryDto.Title,
-        Slug = createCategoryDto.Slug,
-        MetaTag = createCategoryDto.MetaTag,
+            Title = createCategoryDto.Title,
+            Slug = createCategoryDto.Slug,
+            MetaTag = createCategoryDto.MetaTag,
             MetaDescription = createCategoryDto.MetaDescription,
-        ParentId = createCategoryDto.ParentId
+            ParentId = createCategoryDto.ParentId
         });
+        _Context.SaveChanges();
+        return  OperationResult.Success();
+    }
 
     public OperationResult EditCategory(EditCategoryDTO editCategoryDto)
     {
-        throw new NotImplementedException();
     }
 
     public List<CategoryDTO> GatAllCategories()
