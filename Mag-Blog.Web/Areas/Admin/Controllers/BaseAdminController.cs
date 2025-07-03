5cdac2ffafb13d0c2b617728686ab6e5a4fd10a6
BAHMAN <bahmansnights@gmail.com>
2025-07-03 08:44:01 +0430

    add BaseAdminController

---
+++ Mag-Blog.Web/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mag_Blog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BaseAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
