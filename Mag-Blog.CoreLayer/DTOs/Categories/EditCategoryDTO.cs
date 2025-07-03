13724c4672e3039b57d6962aa1ae3d1fda959727
BAHMAN <bahmansnights@gmail.com>
2025-07-03 16:09:09 +0430

    remove parentid from EditCategoryDTO

--- Mag-Blog.CoreLayer/DTOs/Categories/EditCategoryDTO.cs
+++ Mag-Blog.CoreLayer/DTOs/Categories/EditCategoryDTO.cs
@@ -1,8 +1,16 @@
 ﻿namespace Mag_Blog.CoreLayer.DTOs.Categories;
 
-public class EditCategoryDTO:CreateCategoryDTO
+public class EditCategoryDTO
 {
     public int Id { get; set; }
+    public string Title { get; set; }
+    
+    public string Slug { get; set; }
+    
+    
+    public string MetaTag { get; set; }
+    
+    public string MetaDescription { get; set; }
     
     
 }
