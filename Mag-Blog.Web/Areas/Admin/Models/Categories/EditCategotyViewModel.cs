07a3029c3b587626ec219e7af90f503818e304d1
BAHMAN <bahmansnights@gmail.com>
2025-07-05 10:13:17 +0430

    add EditCategoryViewModel.cs

---
+++ Mag-Blog.Web/Areas/Admin/Models/Categories/EditCategotyViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Mag_Blog.Web.Areas.Admin.Models.Categories;
+
+public class EditCategotyViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    
+    public string Slug { get; set; }
+    
+    
+    public string MetaTag { get; set; }
+    
+    public string MetaDescription { get; set; }
+}
