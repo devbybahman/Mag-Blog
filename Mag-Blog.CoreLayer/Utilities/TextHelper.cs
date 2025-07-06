9c4e0730c10f370397cb74bf42812daa496c6b4e
BAHMAN <bahmansnights@gmail.com>
2025-07-06 11:47:59 +0430

    add slug ruls extention method

--- Mag-Blog.CoreLayer/Utilities/TextHelper.cs
+++ Mag-Blog.CoreLayer/Utilities/TextHelper.cs
@@ -1,6 +1,27 @@
 ﻿namespace CodeYad_Blog.CoreLayer.Utilities;
 
-public class TextHelper
+public static class TextHelper
 {
-    
+    public static string ToSlug(this string slug)
+    {
+        return slug.Trim().ToLower()
+            .Replace("~", "")
+            .Replace("!", "")
+            .Replace("@", "")
+            .Replace("#", "")
+            .Replace("$", "")
+            .Replace("%", "")
+            .Replace("^", "")
+            .Replace("<", "")
+            .Replace(">", "")
+            .Replace("&", "")
+            .Replace("*", "")
+            .Replace("(", "")
+            .Replace(")", "")
+            .Replace("_", "-")
+            .Replace(" ", "-")
+            .Replace("/", "")
+            .Replace(@"\", "")
+            .Replace("+", "");
+    }
 }
