846dace8f513f182ed4da9bed60a69907003bcef
BAHMAN <bahmansnights@gmail.com>
2025-07-07 18:07:44 +0430

    Add-Migration add-ImageName-to-Post

--- Mag-Blog.CoreLayer/Services/Posts/PostService.cs
+++ Mag-Blog.CoreLayer/Services/Posts/PostService.cs
@@ -1,6 +1,27 @@
-﻿namespace Mag_Blog.CoreLayer.Services.Posts;
+﻿using CodeYad_Blog.CoreLayer.Utilities;
+using Mag_Blog.CoreLayer.DTOs.Posts;
 
-public class PostService:IPostService
+namespace Mag_Blog.CoreLayer.Services.Posts;
+
+public class PostService : IPostService
 {
-    
+    public OperationResult CreatePost(CreatePostDTO command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OperationResult EditPost(EditPostDTO command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public PostDto GetPostById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSlugExist(string slug)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ Mag-Blog.DataLayer/Migrations/20250707133555_add-ImageName-to-Post.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mag_Blog.DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addImageNametoPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Posts");
+        }
+    }
+}
