f7a3154072754970adec0d1f295e08c4b533a3e1
BAHMAN <bahmansnights@gmail.com>
2025-07-07 17:56:45 +0430

    add CreatePostDTO

---
+++ Mag-Blog.CoreLayer/DTOs/Posts/CreatePostDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Mag_Blog.CoreLayer.DTOs.Posts;
+
+public class CreatePostDTO
+{
+    public int UserId { get; set; }
+    public int CategoryId { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+
+    public string Slug { get; set; }
+}
