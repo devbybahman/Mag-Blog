7afa9442f6a63c4d4a5943a74491bde11d432ee4
BAHMAN <bahmansnights@gmail.com>
2025-07-07 17:53:58 +0430

    initials IPostService methods

--- Mag-Blog.CoreLayer/Services/Posts/IPostService.cs
+++ Mag-Blog.CoreLayer/Services/Posts/IPostService.cs
@@ -1,6 +1,12 @@
-﻿namespace Mag_Blog.CoreLayer.Services.Posts;
+﻿using CodeYad_Blog.CoreLayer.Utilities;
+using Mag_Blog.CoreLayer.DTOs.Categories;
+
+namespace Mag_Blog.CoreLayer.Services.Posts;
 
 public interface IPostService
 {
-    
+    OperationResult CreatePost(CreatePostDTO command);
+    OperationResult EditPost(EditPostDTO command);
+    PostDto GetPostById(int id);
+    bool IsSlugExist(string slug);
 }
