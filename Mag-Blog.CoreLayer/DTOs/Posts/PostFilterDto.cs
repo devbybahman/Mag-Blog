06bd29bb1ef06783e8ee637ddd71dd337993c185
BAHMAN <bahmansnights@gmail.com>
2025-07-07 18:44:31 +0430

    add PostFilterDto properties

--- Mag-Blog.CoreLayer/DTOs/Posts/PostFilterDto.cs
+++ Mag-Blog.CoreLayer/DTOs/Posts/PostFilterDto.cs
@@ -2,5 +2,9 @@
 
 public class PostFilterDto
 {
-    
+    public int PageId { get; set; }
+    public int Take    { get; set; }
+    public List<PostDto> Posts { get; set; }
+    public string Title { get; set; }
+    public string Slug { get; set; }
 }
