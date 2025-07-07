78c55e6795b0866c1bf09aa876b2e8952a59bbca
BAHMAN <bahmansnights@gmail.com>
2025-07-07 18:15:46 +0430

    CreatePost service done

--- Mag-Blog.CoreLayer/Services/Posts/PostService.cs
+++ Mag-Blog.CoreLayer/Services/Posts/PostService.cs
@@ -1,13 +1,33 @@
 ﻿using CodeYad_Blog.CoreLayer.Utilities;
 using Mag_Blog.CoreLayer.DTOs.Posts;
+using Mag_Blog.DataLayer.Context;
+using Mag_Blog.DataLayer.Entities;
 
 namespace Mag_Blog.CoreLayer.Services.Posts;
 
 public class PostService : IPostService
 {
+    private readonly BlogCobtext _context;
+
+    public PostService(BlogCobtext context)
+    {
+        _context = context;
+    }
     public OperationResult CreatePost(CreatePostDTO command)
     {
-        throw new NotImplementedException();
+        _context.Posts.Add(new Post()
+        {
+            Title = command.Title,
+             IsDeleted = false,
+            Description = command.Description,
+            Slug = command.Slug.ToSlug(),
+         CategoryId   = command.CategoryId,
+         UserId = command.UserId,
+         Visited = 0
+        });
+        _context.SaveChanges();
+        return OperationResult.Success();
+
     }
 
     public OperationResult EditPost(EditPostDTO command)
