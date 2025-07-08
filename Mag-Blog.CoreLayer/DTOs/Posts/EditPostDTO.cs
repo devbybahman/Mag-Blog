a21cfbbea8b1d83d2f0bb9be54df963ea4833370
BAHMAN <bahmansnights@gmail.com>
2025-07-08 19:33:29 +0430

    add SubCategoryId

--- Mag-Blog.CoreLayer/DTOs/Posts/EditPostDTO.cs
+++ Mag-Blog.CoreLayer/DTOs/Posts/EditPostDTO.cs
@@ -4,7 +4,7 @@ public class EditPostDTO
 {
     public int PostId { get; set; }
     public int CategoryId { get; set; }
-    
+    public int? SubCategoryId { get; set; }
     public string Title { get; set; }
     
     public string Description { get; set; }
