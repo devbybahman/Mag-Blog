bdeb5632e1a00d3c4877a38b803297d8c3394571
BAHMAN <bahmansnights@gmail.com>
2025-07-08 15:57:53 +0430

    add UserUtil for GetUserId from Claims

---
+++ Mag-Blog.CoreLayer/Utilities/UserUtil.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace CodeYad_Blog.CoreLayer.Utilities;
+
+public static class UserUtil
+{
+    public static int GetUserId(this ClaimsPrincipal principal)
+    {
+        if (principal==null)
+        {
+            throw new ArgumentNullException(nameof(principal));
+        }
+
+        return int.Parse(principal.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+    }
+}
