be24c18e412529383cf5de146bdcb22bb6ca3fa7
BAHMAN <bahmansnights@gmail.com>
2025-07-08 12:53:26 +0430

    update CreatePostViewModel

--- Mag-Blog.Web/Areas/Admin/Models/Posts/CreatePostViewModel.cs
+++ Mag-Blog.Web/Areas/Admin/Models/Posts/CreatePostViewModel.cs
@@ -7,9 +7,22 @@ namespace Mag_Blog.Web.Areas.Admin.Models.Posts;
 
 public class CreatePostViewModel
 {    
+    [Display(Name = "انتخاب دسته بندی")]
+    [System.ComponentModel.DataAnnotations.Required(ErrorMessage ="لطفا {0} را وارد کنید ")]
     public int CategoryId { get; set; }
+    [Display(Name = "انتخاب زیر دسته بندی")]
+    [System.ComponentModel.DataAnnotations.Required(ErrorMessage ="لطفا {0} را وارد کنید ")]
     public int? SubCategoryId { get; set; }
+    [Display(Name = "عنوان")]
+    [System.ComponentModel.DataAnnotations.Required(ErrorMessage ="لطفا {0} را وارد کنید ")]
     public string Title { get; set; }
+    [Display(Name = "توضیحات")]
+    [System.ComponentModel.DataAnnotations.Required(ErrorMessage ="لطفا {0} را وارد کنید ")]
     public string Description { get; set; }
+    [Display(Name = "slug")]
+    [System.ComponentModel.DataAnnotations.Required(ErrorMessage ="لطفا {0} را وارد کنید ")]
     public string Slug { get; set; }
+    [Display(Name = "عکس")]
+    [System.ComponentModel.DataAnnotations.Required(ErrorMessage ="لطفا {0} را وارد کنید ")]
+    public IFormFile ImageFile { get; set; }
 }
