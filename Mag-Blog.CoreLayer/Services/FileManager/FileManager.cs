26bc9054c382ea8510878f6866c556cf55e785b5
BAHMAN <bahmansnights@gmail.com>
2025-07-08 15:17:27 +0430

    init IFileManager for save files in server

---
+++ Mag-Blog.CoreLayer/Services/FileManager/FileManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Mag_Blog.CoreLayer.Services.FileManager;
+
+public class FileManager:IFileManager
+{
+    public string SaveFile(IFormFile file, string savepath)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Mag-Blog.CoreLayer/Services/FileManager/IFileManager.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Mag_Blog.CoreLayer.Services.FileManager;
+
+public interface IFileManager
+{
+    string SaveFile(IFormFile file, string savepath);
+}
