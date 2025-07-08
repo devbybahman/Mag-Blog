dcd7389e6d2df36ca3de4c9e4b0538103d8f4306
BAHMAN <bahmansnights@gmail.com>
2025-07-08 11:59:37 +0430

    add index of Post

--- Mag-Blog.Web/Areas/Admin/Controllers/PostController.cs
+++ Mag-Blog.Web/Areas/Admin/Controllers/PostController.cs
@@ -1,13 +1,20 @@
+using Mag_Blog.CoreLayer.Services.Posts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mag_Blog.Web.Areas.Admin.Controllers
-{
+{[Area("Admin")]
     public class PostController : BaseAdminController
     {
-        // GET: PostController
-        public ActionResult Index()
+        private readonly IPostService _service;
+
+        public PostController(IPostService service)
         {
-            return View();
+            _service = service;
+        }
+        public ActionResult Index(int pageid=1,string title="",string categorySlug="")
+        {
+            var r = _service.GetPostByFilter(pageid, title, categorySlug, 20);
+            return View(r);
         }
 
     }
