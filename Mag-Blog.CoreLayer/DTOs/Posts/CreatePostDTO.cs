cc3e89e6ab91b71efb157386f61ddea5ce8cadf6
BAHMAN <bahmansnights@gmail.com>
2025-07-08 12:34:16 +0430

    update CreatePostDTO

--- Mag-Blog.CoreLayer/DTOs/Posts/CreatePostDTO.cs
+++ Mag-Blog.CoreLayer/DTOs/Posts/CreatePostDTO.cs
@@ -1,13 +1,16 @@
-﻿namespace Mag_Blog.CoreLayer.DTOs.Posts;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Mag_Blog.CoreLayer.DTOs.Posts;
 
 public class CreatePostDTO
 {
     public int UserId { get; set; }
     public int CategoryId { get; set; }
-    
+    public int? SubCategoryId { get; set; }
     public string Title { get; set; }
     
     public string Description { get; set; }
 
     public string Slug { get; set; }
+    public IFormFile ImageFile { get; set; }
 }
--- Mag-Blog.Web/Areas/Admin/Models/Posts/CreatePostViewModel.cs
+++ Mag-Blog.Web/Areas/Admin/Models/Posts/CreatePostViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace Mag_Blog.Web.Areas.Admin.Models.Posts;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Mag_Blog.CoreLayer.DTOs.Categories;
+using Microsoft.Build.Framework;
+
+namespace Mag_Blog.Web.Areas.Admin.Models.Posts;
 
 public class CreatePostViewModel
-{
-    
+{    
+    public int CategoryId { get; set; }
+    public int? SubCategoryId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Slug { get; set; }
 }
